faae3ed1e90b67dc4086ef100e2e904f8c933e13
Hairunisa <143789547+hairunisa0929@users.noreply.github.com>
2024-07-09 16:30:58 +0700

    add function exercise

---
+++ javascript/js-function.js
@@ -0,0 +1,17 @@
+// Buatlah function getTriangleArea untuk menghitung luas segitiga
+// Function tersebut menerima 2 parameter, yaitu nilai alas dan tinggi
+// Buat persamaan untuk menghitung luas segitiga dalam function tersebut
+// Tampilkan hasil perhitungan dalam console
+// Panggil function dan masukkan nilai alas dan tinggi sebagai parameter
+
+function getTriangleArea() {
+
+} 
+
+// Contoh
+// function getTriangleArea() {
+//     const area = 1/2 * a * b;
+//     console.log(area);
+// } 
+
+// getTriangleArea(2,5);
