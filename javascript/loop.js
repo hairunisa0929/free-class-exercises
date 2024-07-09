bd1e560039242102c779bc17306b188f19c2d943
Hairunisa <143789547+hairunisa0929@users.noreply.github.com>
2024-07-09 17:08:40 +0700

    add exercise lop

---
+++ javascript/loop.js
@@ -0,0 +1,9 @@
+let students = ["Sandy", "Larry", "Eugene"];
+
+// Tampilkan nama-nama student tersebut pada console dengan menggunakan loop
+
+
+// Contoh
+// for (let i = 0; i < students.length; i++) {
+//   console.log(students[i]);
+// }
