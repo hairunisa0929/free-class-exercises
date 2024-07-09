9db9fa6201636e10f02da8afaa3b3910cac7af62
Hairunisa <143789547+hairunisa0929@users.noreply.github.com>
2024-07-09 16:56:48 +0700

    add exercise ternary operator

---
+++ javascript/ternary-operator.js
@@ -0,0 +1,15 @@
+let grade = 80;
+
+// Buatlah kondisi menggunakan ternary operator
+// Jika nilai lebih dari atau sama dengan 40, maka akan mengembalikan "pass", selain itu akan mengembalikan "fail"
+let result = grade > 20 ? "" : "";
+
+// Kemudian tampilkan result dalam console
+console.log();
+
+
+// let grade = 80;
+
+// let result = (grade >= 40) ? 'pass' : 'fail';
+
+// console.log(result);
