3371472b2e7ca12044a49c17dd901b558b60d94e
Hairunisa <143789547+hairunisa0929@users.noreply.github.com>
2024-07-10 08:46:10 +0700

    add exercise array of object

---
+++ javascript/object.js
@@ -0,0 +1,33 @@
+let cats = [
+  {
+    id: 1,
+    name: "snowy",
+    color: "white",
+    breed: "ragdoll",
+    age: 2,
+  },
+  {
+    id: 2,
+    name: "simba",
+    color: "orange",
+    breed: "abyssinian",
+    age: 12,
+  },
+];
+
+// Tambahkan object baru ke dalam array of object tersebut
+// Tampilkan array cats setelah object baru ditambahkan
+
+
+
+// Contoh
+// const cat = {
+//   id: 3,
+//   name: "taesan",
+//   color: "black",
+//   breed: "persian",
+//   age: 5,
+// };
+
+// cats.push(cat);
+// console.log(cats);
