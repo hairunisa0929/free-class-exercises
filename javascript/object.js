3d025039f23ef0074914c89a16a015866430a8f0
Hairunisa <143789547+hairunisa0929@users.noreply.github.com>
2024-08-02 15:17:32 +0700

    update object challengen

--- javascript/object.js
+++ javascript/object.js
@@ -1,25 +1,7 @@
-let cats = [
-  {
-    id: 1,
-    name: "snowy",
-    color: "white",
-    breed: "ragdoll",
-    age: 2,
-  },
-  {
-    id: 2,
-    name: "simba",
-    color: "orange",
-    breed: "abyssinian",
-    age: 12,
-  },
-];
-
-
-// Tampilkan name dari masing-masing elemen dalam array cats menggunakan for of
-
-
-// contoh
-for (let cat of cats) {
-  console.log(cat.name)
+let cat = {
+  id: 1,
+  name: "Kimmy",
+  color: "Orange"
 }
+
+console.log(cat.color);
